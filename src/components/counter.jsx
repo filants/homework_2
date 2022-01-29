d2fa778b31bf01013f2d51067057315bad9911e0
filants <antons.filipovs@gmail.com>
2022-01-29 22:10:07 +0100

    added counter

---
+++ src/components/counter.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const formCount = () => {
+    return count === 0 ? "Ноль" : count;
+  };
+
+  const getBadgeClasses = () => {
+    let classes = "badge m-2 bg-";
+    classes += count === 0 ? "danger" : "primary";
+    return classes;
+  };
+
+  const handleIncrement = (productId) => {
+    console.log(productId);
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    if (count > 0) return setCount(count - 1);
+  };
+
+  return (
+    <>
+      <span className={getBadgeClasses()}>{formCount()}</span>
+      <button
+        onClick={() => handleIncrement({ id: 1 })}
+        className="btn btn-secondary btn-sm"
+      >
+        Increment
+      </button>
+      <button onClick={handleDecrement} className="btn btn-secondary btn-sm">
+        Decrement
+      </button>
+    </>
+  );
+};
+
+export default Counter;
